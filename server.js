eaf2862ca74d72b17e43ae7d7c6cec563638ca6d
mahmoudAcm <mahmoud.acm.123@gamil.com>
2020-10-19 10:25:14 +0200

    finshed cards and style

--- server.js
+++ server.js
@@ -13,19 +13,32 @@ const app = express();
 /* Middleware*/
 app.use(express.json());
 
-// Cors for cross origin allowance
+// Cors for cross origin allowance if it is not the same orign our case is
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 //endpoint for geeting weather using zip code or city name
 app.get('/weather/:zipCity', async (req, res) => {
-   const { zipCity } =  req.params; 
-   const { current } = await request({
-       uri: `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${zipCity}`,
-       json: true, 
-   });
-   res.send({current});
+   try { 
+        const { zipCity } =  req.params; 
+        const { current, location } = await request({
+            uri: `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${zipCity}`,
+            json: true, 
+        });
+        
+        res.send({ 
+            temp: current.temp_c,
+            condition:current.condition,
+            city: location.name,
+            country: location.country
+        });
+   } catch(error) {
+        res.send({
+            code: 400,
+            error: error.message
+        }) ;
+   }
 });
 
 // Setup Server
--- website/app.js
+++ website/app.js
@@ -1,5 +1,7 @@
 /* Global Variables */
 let zipCity = null;
+let view = document.getElementById("main-view");
+let loader = document.getElementById("loader");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
@@ -20,11 +22,27 @@ const getData = () => {
             return;
         }
 
+        loader.style.opacity = '1';
+
         //use the api to get the weather
         fetch(`/weather/${zipCity}`).then(res => res.json()).
-        then(data => {
-            console.log(data);
+        then(({city, temp, condition, country, ...rest}) => {
+            loader.style.opacity = '0';
+            if(rest.code === 400){
+                alert(rest.error);    
+                return;
+            }
+            view.innerHTML += `
+                <div class="card">
+                <h3 class="city">${city} <sup>${country}</sup></h3>
+                <h1 class="temp">${temp}<sup>°C</sup></h1>
+                <img src="https:${condition.icon}" alt="temp" draggable="false">
+                <p class="condition">${condition.text}</p>
+                </div>
+            `;
         })
+
+        document.getElementById("zip-city").value = "";         
     });
 };
 
